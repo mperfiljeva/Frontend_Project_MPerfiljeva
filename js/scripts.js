c3cb3660e64b2e6e1aa05ca6a6355d5295fd361a
mperfiljeva <50054189+mperfiljeva@users.noreply.github.com>
2019-05-25 16:08:51 +0200

    add responsive hamburger menu
    Menu closes on the click outside and vanishes on window resize.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
 console.log("Welcome to my Portfolio website!")
+
+$(window).resize(checkWindowWidth);
+function checkWindowWidth() {
+    var width = $(window).width();
+    var x = document.getElementById('mynavtoggle');
+    if (width >= 640) {
+        x.className = 'navtoggle';
+    }
+}
+
+function menuToggle() {
+  var x = document.getElementById('mynavtoggle');
+if (x.className === 'navtoggle') {
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
+$(document).click(function(event) {
+  $target = $(event.target);
+  var x = document.getElementById('mynavtoggle');
+  if(!$target.closest('#mynavtoggle').length &&
+  $('#mynavtoggle').is(":visible")) {
+    x.className = 'navtoggle';
+  }
+});
