8673e4451c259fabca5fb0daf59222c01023e73b
mperfiljeva <50054189+mperfiljeva@users.noreply.github.com>
2019-06-08 21:50:58 +0200

    improve code quality and syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
-console.log("Welcome to my Portfolio website!")
 
-$(window).resize(checkWindowWidth);
-function checkWindowWidth() {
-    var width = $(window).width();
-    var x = document.getElementById('mynavtoggle');
-    if (width >= 640) {
-        x.className = 'navtoggle';
-    }
-}
+/* global $ */
 
+console.log('Welcome to my Portfolio website!')
 
-function menuToggle() {
-  var x = document.getElementById('mynavtoggle');
-  var width = $(window).width();
-  if (width >= 640) {}
-  else if (x.className === 'navtoggle')
-  {x.className += ' responsive';}
-  else {x.className = 'navtoggle';
+$(window).resize(checkWindowWidth)
+
+function checkWindowWidth () {
+  var width = $(window).width()
+  var x = document.getElementById('mynavtoggle')
+  if (width >= 640) {
+    x.className = 'navtoggle'
   }
 }
 
-
-$(document).click(function(event) {
-  $target = $(event.target);
-  var x = document.getElementById('mynavtoggle');
-  if(!$target.closest('#mynavtoggle').length &&
-  $('#mynavtoggle').is(":visible")) {
-    x.className = 'navtoggle';
+function menuToggle () {
+  var x = document.getElementById('mynavtoggle')
+  var width = $(window).width()
+  if (width >= 640) {} else if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
   }
-});
+}
+
+$(document).click(function (event) {
+  var $target = $(event.target)
+  var x = document.getElementById('mynavtoggle')
+  if (!$target.closest('#mynavtoggle').length &&
+    $('#mynavtoggle').is(':visible')) {
+    x.className = 'navtoggle'
+  }
+})
